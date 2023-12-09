d97b433fcb683ad40a0fa1f14e622334ad16dd4d
Ayush Tiwari <101995061+aaayushh7@users.noreply.github.com>
2023-12-09 18:52:21 +0530

    mapped navIcons

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+
+const navIcons = [
+  {src: '/assets/icons/search.svg', alt: 'search'},
+  {src: '/assets/icons/black-heart.svg', alt: 'heart'},
+  {src: '/assets/icons/user.svg', alt: 'user'},
+]
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <header className='w-full'>
+      <nav className='nav'>
+        <Link href="/" className='flex items-center gap-1'>
+          <Image src="/assets/icons/logo.svg"
+          width={27}
+          height={27}
+          alt='logo'/>
+
+          <p className='nav-logo'>
+            Track<span className='text-primary'>Hub</span>
+
+          </p>
+        </Link>
+        <div className='flex items-center gap-5'>
+          {navIcons.map((icon) => (
+              <Image
+              key={icon.alt}
+              src={icon.src}
+              alt={icon.alt}
+              width={28}
+              height={28}
+              className='object-contain'/>
+          ))}
+
+        </div>
+
+      </nav>
+    </header>
   )
 }
 
