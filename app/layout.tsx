03c09ca735f38820c5d6e8304574e4bffd31c454
Ayush Tiwari <101995061+aaayushh7@users.noreply.github.com>
2023-12-09 17:05:58 +0530

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Inter, Space_Grotesk } from 'next/font/google'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
+const spaceGrotesk = Space_Grotesk({ 
+  subsets: ['latin'], weight: ['300','400','500','600','700']})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
