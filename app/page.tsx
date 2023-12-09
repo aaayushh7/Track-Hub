400ed27e7988daf25484145f8e2c5604e8b6f303
Ayush Tiwari <101995061+aaayushh7@users.noreply.github.com>
2023-12-10 00:37:07 +0530

    Arraw img

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ const Home = () => {
       <section className="px-6 md:px-20 py-24">
         <div className="flex max-xl:flex-col gap-16">
           <div className="flex flex-col justify-center">
-            <p className="small-text">Smart Shopping Starts Here:
+            <p className="small-text">Smart Shopping Starts Here
               <Image
                 src="/assets/icons/arrow-right.svg"
                 alt="arrow-right"
