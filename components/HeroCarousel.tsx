86061a5b22cd0a0b64934f8eb2cea19925940f9d
Ayush Tiwari <101995061+aaayushh7@users.noreply.github.com>
2023-12-09 19:38:25 +0530

    Third party carousel

--- components/HeroCarousel.tsx
+++ components/HeroCarousel.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+"use client"
+
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import Image from "next/image";
+
+const heroImages = [
+    {imgUrl: '/assets/images/hero-1.svg', alt: 'smartwatch'},
+    {imgUrl: '/assets/images/hero-2.svg', alt: 'bag'},
+    {imgUrl: '/assets/images/hero-3.svg', alt: 'lamp'},
+    {imgUrl: '/assets/images/hero-4.svg', alt: 'air fryer'},
+    {imgUrl: '/assets/images/hero-5.svg', alt: 'chair'},
+
+]
+
 
 const HeroCarousel = () => {
-  return (
-    <div>HeroCarousel</div>
-  )
+    return (
+        <div>
+            <Carousel 
+            showThumbs={false}
+            autoPlay
+            infiniteLoop
+            interval={2000}
+            showArrows={false}
+            showStatus={false}
+            >
+                {heroImages.map((image) => (
+                    <Image 
+                    src={image.imgUrl}
+                    alt={image.alt}
+                    width={484}
+                    height={484}
+                    className="object-contain"
+                    key={image.alt}/>
+                ))}
+            </Carousel>
+        </div>
+    )
 }
 
 export default HeroCarousel
