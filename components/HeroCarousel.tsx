3d390bd03cc78cbe5047e2f191cdf998da826eb8
Ayush Tiwari <101995061+aaayushh7@users.noreply.github.com>
2023-12-09 20:22:30 +0530

    new tailwind custom

--- components/HeroCarousel.tsx
+++ components/HeroCarousel.tsx
@@ -16,7 +16,7 @@ const heroImages = [
 
 const HeroCarousel = () => {
     return (
-        <div>
+        <div className="hero-carousel">
             <Carousel 
             showThumbs={false}
             autoPlay
@@ -35,6 +35,14 @@ const HeroCarousel = () => {
                     key={image.alt}/>
                 ))}
             </Carousel>
+
+            <Image
+            src="/assets/icons/hand-drawn-arrow.svg"
+            alt="Arrow"
+            width={175}
+            height={175}
+            className="max-xl:hidden absolute -left-[15%] bottom-0 z-0"
+            />
         </div>
     )
 }
