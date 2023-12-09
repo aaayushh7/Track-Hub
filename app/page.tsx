743303079137c8da0ae58bc7aafa985606551f1a
Ayush Tiwari <101995061+aaayushh7@users.noreply.github.com>
2023-12-09 19:08:10 +0530

    trending seciton

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,41 @@
+import Image from 'next/image'
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <section className="px-6 md:px-20 py-24 border-2 border-red-500">
+        <div className="flex max-xl:flex-col gap-16">
+          <div className="flex flex-col justify-center">
+            <p className="small-text">Smart Shopping Starts Here:
+              <Image
+                src="/assets/icons/arrow-right.svg"
+                alt="arrow-right"
+                width={16}
+                height={16}
+              ></Image>
+            </p>
+            <h1 className='head-text'>
+              Unleash the power of
+              <span className='text-primary'> TrackHub</span>
+            </h1>
+            <p className='mt-6'>
+              Powerful, self-serve product and growth analytics to help you convert, engage, and retain more.
+            </p>
+            Searchbar
+          </div>
+          HeroCarousel
+        </div>
+
+      </section>
+      <section className='trending-section'>
+        <h2 className='section-text'>Trending</h2>
+        <div className='flex flex-wrap gap-x-8 gap-y-16'>
+          {['Apple Iphone 15','Book','Sneaker'].map((product) => (
+            <div>{product}</div>
+          ))}
+        </div>
+      </section>
+    </>
   )
 }
 
