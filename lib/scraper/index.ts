0bf51002ed915eed7381cd8821160545bd5e42fc
Ayush Tiwari <101995061+aaayushh7@users.noreply.github.com>
2023-12-10 13:41:19 +0530

    retrieved data from all tags

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import * as cheerio from "cheerio"
-import { extractPrice } from "../utils";
+import { extractCurrency, extractDescription, extractPrice } from "../utils";
 
 export async function scrapeAmazonProduct(url: string) {
     if (!url) return;
@@ -39,8 +39,40 @@ export async function scrapeAmazonProduct(url: string) {
         $('#priceblock_dealprice'),
         $('.a-size-base.a-color-price')
        )
-       console.log({title, currentPrice, originalPrice})
+       const outOfStock = $('$availability span').text().trim().toLowerCase() === 'currently unavailable';
+       const images = 
+       $('#imgBlkFront').attr('data-a-dynamic-image') ||
+       $('#landingImage').attr('data-a-dynamic-image') ||
+       '{}'
 
+       const imageUrls = Object.keys(JSON.parse(images))
+
+       const currency = extractCurrency($('.a-price-symbol'))
+       const discountRate = $('.savingsPercentage').text().replace(/[-%]/g,'');
+
+       const description = extractDescription($)
+
+        //Construct data object with scraped data
+        const data = {
+            url,
+            currency: currency || "$",
+            image: imageUrls[0],
+            title,
+            currentPrice: Number(currentPrice) ||Number(originalPrice),
+            originalPrice: Number(originalPrice) || Number(currentPrice),
+            priceHistory: [],
+            discountRate: Number(discountRate),
+            category: 'category',
+            reviewsCount: 100,
+            stars: 4,
+            isOutOfStock: outOfStock, 
+            description,
+            lowestPrice: Number(currentPrice) ||  Number(originalPrice),
+            highestPrice: Number(originalPrice) || Number(currentPrice),
+            average: Number(currentPrice) || Number(originalPrice)
+
+        }
+        return data;
        
     } catch (error: any) {
         throw new Error(`error in fetching data : ${error.message}`)
