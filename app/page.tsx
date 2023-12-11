393b774724e68b055b0914bd53cda732fda81f72
Ayush Tiwari <101995061+aaayushh7@users.noreply.github.com>
2023-12-12 02:24:30 +0530

    previous call section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import Image from 'next/image'
 import Searchbar from '@/components/searchbar'
 import HeroCarousel from '@/components/HeroCarousel'
+import { getAllProducts } from '@/lib/actions'
+import ProductCard from '@/components/ProductCard'
 
-const Home = () => {
+const Home = async () => {
+  const allProducts = await getAllProducts();
+
   return (
     <>
       <section className="px-6 md:px-20 py-24">
@@ -30,10 +34,10 @@ const Home = () => {
 
       </section>
       <section className='trending-section'>
-        <h2 className='section-text'>Trending</h2>
+        <h2 className='section-text'>Your previous Search</h2>
         <div className='flex flex-wrap gap-x-8 gap-y-16 text-gray-400'>
-          {['Apple Iphone 15','Book','Sneaker'].map((product) => (
-            <div>{product}</div>
+          {allProducts?.map((product) => (
+            <ProductCard key={product._id} product={product}/>
           ))}
         </div>
       </section>
