a38be2d27c6a94c956202083ded98fd3acdbe070
Ayush Tiwari <101995061+aaayushh7@users.noreply.github.com>
2023-12-12 01:33:41 +0530

    ID extraction

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,13 +1,62 @@
 "use server"
+
+import { revalidatePath } from "next/cache";
+import { connect } from "http2";
 import { scrapeAmazonProduct } from "../scraper";
+import { connectToDB } from "../mongoose";
+import Product from "../models/product.model";
+import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
 
 export async function scrapAndStoreProduct(productUrl: string) {
     if(!productUrl) return;
 
     try {
+
+        connectToDB();
         const scrapedProduct = await scrapeAmazonProduct(productUrl);
         if(!scrapedProduct) return;
+
+        let product = scrapedProduct;
+        const existingProduct = await Product.findOne({url: scrapedProduct.url});
+
+        if (existingProduct){
+            const updatedPriceHistory: any = [
+                ...existingProduct.priceHistory,
+                { price: scrapedProduct.currentPrice }
+            ]
+
+            product = {
+                ...scrapedProduct,
+                priceHistory: updatedPriceHistory,
+                lowestPrice: getLowestPrice(updatedPriceHistory),
+                highestPrice: getHighestPrice(updatedPriceHistory),
+                averagePrice: getAveragePrice(updatedPriceHistory),
+            }
+        }
+
+        const newProduct = await Product.findOneAndUpdate(
+            {url: scrapedProduct.url},
+            product, 
+            { upsert: true, new: true }
+        );
+
+        revalidatePath(`/products/${newProduct._id}`);
+
     } catch (error: any) {
         throw new Error(`failed to create/update this product : ${error.message}`)
+    }
+}
+
+export async function getProductById(productId: string){
+    try {
+        connectToDB();
+
+        const product = await Product.findOne({_id: productId});
+
+        if(!product) return null;
+
+        return product;
+    } catch (error) {
+        console.log(error);
     }
 }
