067076a4b006eb860f6b0429942e5285c5742ba7
Ayush Tiwari <101995061+aaayushh7@users.noreply.github.com>
2023-12-18 01:50:32 +0530

    usability

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ const Home = async () => {
               <span className='text-primary'> TrackHub</span>
             </h1>
             <p className='mt-6 text-gray-100'>
-              Powerful, self-serve product and growth analytics to help you convert, engage, and retain more.
+              Track your favourite producy just by a single click, Search → scroll to your product → Track → Register and done! Get notified when it reaches it's lowest price or comes back in stock!
             </p>
             <Searchbar/>
           </div>
