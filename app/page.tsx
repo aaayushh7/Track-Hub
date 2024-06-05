365911a965484062658563664bedf869676db249
Ayush Tiwari <101995061+aaayushh7@users.noreply.github.com>
2024-06-05 11:40:11 +0530

    minor

--- app/page.tsx
+++ app/page.tsx
@@ -27,9 +27,9 @@ const Home = async () => {
             <p className='mt-6 text-gray-100'>
               Track your favourite product just by a single click, Search → scroll to your product → Track → Register and done! Get notified when it reaches it's lowest price or comes back in stock!
             </p>
-            <Searchbar/>
+            <Searchbar />
           </div>
-          <HeroCarousel/>
+          <HeroCarousel />
         </div>
 
       </section>
@@ -37,33 +37,33 @@ const Home = async () => {
         <h2 className='section-text'>Trending</h2>
         <div className='flex flex-wrap gap-x-8 gap-y-16 text-gray-400'>
           {allProducts?.slice() // Create a shallow copy of the array to avoid mutating the original
-    .reverse().map((product) => (
-            <ProductCard key={product._id} product={product}/>
-          ))}
+            .reverse().map((product) => (
+              <ProductCard key={product._id} product={product} />
+            ))}
         </div>
-        </section>
+      </section>
 
 
-<footer className="bg-white rounded-lg shadow mt-4 dark:bg-gray-800">
-    <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-      <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline">Pranjal Tiwari</a>. All Rights Reserved.
-    </span>
-    <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
-        <li>
-            <a href="https://www.linkedin.com/in/pranjal-tiwari-01a516231/" className="hover:underline me-4 md:me-6">About</a>
-        </li>
-        <li>
-            <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
-        </li>
-        
-        <li>
-            <a href="https://www.linkedin.com/in/pranjal-tiwari-01a516231/" className="hover:underline">Contact</a>
-        </li>
-    </ul>
-    </div>
-</footer>
+      <footer className="bg-white rounded-lg shadow mt-4 dark:bg-gray-800">
+        <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline"></a>. All Rights Reserved.
+          </span>
+          <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+            <li>
+              <a href="https://www.linkedin.com/in/pranjal-tiwari-01a516231/" className="hover:underline me-4 md:me-6">About</a>
+            </li>
+            <li>
+              <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
+            </li>
 
-     
+            <li>
+              <a href="https://www.linkedin.com/in/pranjal-tiwari-01a516231/" className="hover:underline">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </footer>
+
+
     </>
   )
 }
