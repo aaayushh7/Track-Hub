4b353976e71c12d4920774b22040e65b5c55015e
Ayush Tiwari <101995061+aaayushh7@users.noreply.github.com>
2024-06-05 11:43:12 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -46,18 +46,18 @@ const Home = async () => {
 
       <footer className="bg-white rounded-lg shadow mt-4 dark:bg-gray-800">
         <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline"></a>. All Rights Reserved.
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="" className="hover:underline"></a>. All Rights Reserved.
           </span>
           <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
             <li>
-              <a href="https://www.linkedin.com/in/pranjal-tiwari-01a516231/" className="hover:underline me-4 md:me-6">About</a>
+              <a href="" className="hover:underline me-4 md:me-6">About</a>
             </li>
             <li>
               <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
             </li>
 
             <li>
-              <a href="https://www.linkedin.com/in/pranjal-tiwari-01a516231/" className="hover:underline">Contact</a>
+              <a href="" className="hover:underline">Contact</a>
             </li>
           </ul>
         </div>
